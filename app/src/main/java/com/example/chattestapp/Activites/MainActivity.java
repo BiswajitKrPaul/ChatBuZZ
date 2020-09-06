f305112bab996f2eaf7a2a55faea1329ddda4ef3
biswajitkmrpl46@gmail.com <iloveutua96>
2020-09-06 23:08:51 +0530

    Commited from Laptop

--- app/src/main/java/com/example/chattestapp/Activites/MainActivity.java
+++ app/src/main/java/com/example/chattestapp/Activites/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.chattestapp.Activites;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.chattestapp.R;
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MainActivity","Just Testing");
     }
 }
