1e34a0b9361ec7c381f0c36c16865f99418967f5
BiswajitKrPaul <biswajitkmrpl46@gmail.com>
2020-09-07 20:20:30 +0530

    Changes done on App from PC

--- app/src/main/java/com/example/chattestapp/Activites/ChatList.java
+++ app/src/main/java/com/example/chattestapp/Activites/ChatList.java
@@ -2,7 +2,11 @@ package com.example.chattestapp.Activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.chattestapp.R;
 import com.example.chattestapp.Utils.ChatUtils;
@@ -18,5 +22,27 @@ public class ChatList extends AppCompatActivity {
         setContentView(R.layout.activity_chat_list);
         mAuth = FirebaseAuth.getInstance();
         ChatUtils.maketoast(ChatList.this, "Welcome back : " + mAuth.getCurrentUser().getEmail());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.chatlist_logout:
+                mAuth.signOut();
+                Intent intent = new Intent(ChatList.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/chattestapp/Activites/MainActivity.java
+++ app/src/main/java/com/example/chattestapp/Activites/MainActivity.java
@@ -1,31 +1,37 @@
 package com.example.chattestapp.Activites;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.example.chattestapp.R;
 import com.example.chattestapp.Utils.ChatUtils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText et_login_email, et_login_password;
+    String email, password;
     FirebaseUser user;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intiliatize();
+        initialize();
         user = FirebaseAuth.getInstance().getCurrentUser();
+        mAuth = FirebaseAuth.getInstance();
         if (user != null) {
             Intent intent = new Intent(MainActivity.this, ChatList.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -34,13 +40,46 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void intiliatize() {
+    private void fetchFields() {
+        email = et_login_email.getText().toString();
+        password = et_login_password.getText().toString();
+    }
+
+    private void initialize() {
         et_login_email = findViewById(R.id.Login_Email);
         et_login_password = findViewById(R.id.Login_Password);
     }
 
+    private boolean checkFields() {
+        if (TextUtils.isEmpty(email)) {
+            et_login_email.setError("Email is blank");
+            return false;
+        }
+        if (TextUtils.isEmpty(password)) {
+            et_login_password.setError("Password is blank");
+            return false;
+        }
+        return true;
+    }
+
     public void login(View view) {
-        ChatUtils.maketoast(getApplicationContext(), "In Progress Yet to implement the functionality");
+
+        fetchFields();
+
+        if (checkFields()) {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Intent intent = new Intent(MainActivity.this, ChatList.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    } else {
+                        ChatUtils.maketoast(MainActivity.this, "Login Failed due to : " + task.getException().getMessage());
+                    }
+                }
+            });
+        }
     }
 
     public void Register(View view) {
