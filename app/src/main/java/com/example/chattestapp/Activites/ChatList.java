3add156c54c5ec4cba713d7ceaf62154c83cc969
BiswajitKrPaul <biswajitkmrpl46@gmail.com>
2020-09-12 22:55:01 +0530

    bug fix for mAuth

--- app/src/main/java/com/example/chattestapp/Activites/ChatList.java
+++ app/src/main/java/com/example/chattestapp/Activites/ChatList.java
@@ -37,13 +37,14 @@ public class ChatList extends AppCompatActivity {
             Intent intent = new Intent(ChatList.this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
+        } else {
+            viewPager = findViewById(R.id.chatlist_viewpager);
+            tabLayout = findViewById(R.id.chatlist_tablayout);
+            mainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+            viewPager.setAdapter(mainViewPagerAdapter);
+            tabLayout.setupWithViewPager(viewPager);
+            LoadMenuBar();
         }
-        viewPager = findViewById(R.id.chatlist_viewpager);
-        tabLayout = findViewById(R.id.chatlist_tablayout);
-        mainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        viewPager.setAdapter(mainViewPagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
-        LoadMenuBar();
     }
 
     private void LoadMenuBar() {
