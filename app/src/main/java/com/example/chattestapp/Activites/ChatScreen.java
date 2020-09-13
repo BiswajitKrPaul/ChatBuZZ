3df6a50d7d7020f6ea05d2057a7fdb72ec54f1d0
BiswajitKrPaul <biswajitkmrpl46@gmail.com>
2020-09-13 13:58:08 +0530

    Fixed Chat Screen layout

--- app/src/main/java/com/example/chattestapp/Activites/ChatScreen.java
+++ app/src/main/java/com/example/chattestapp/Activites/ChatScreen.java
@@ -81,6 +81,20 @@ public class ChatScreen extends AppCompatActivity {
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
+
+        recyclerView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View view, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                if (bottom < oldBottom) {
+                    recyclerView.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            recyclerView.scrollToPosition(chatScreenAdapter.getItemCount() - 1);
+                        }
+                    }, 100);
+                }
+            }
+        });
     }
 
     private void LoadMoreData() {
